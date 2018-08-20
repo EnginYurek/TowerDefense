5f3c1f01bdf4b8affddddb0c85ca586164beb648
Engin Yurek <yurekengin@gmail.com>
2018-08-20 15:42:02 +0300

    Add Turret
    Turret Prefab is added.
    Dedect and lock to enemy is done.

---
+++ Assets/Turret.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turret : MonoBehaviour {
+
+    private Transform target;
+    public float range = 15f;
+
+    public string enemyTag = "Enemy";
+
+    public float turnSpeed = 10f;
+
+    public Transform partToRotate;
+	// Use this for initialization
+	void Start () {
+        InvokeRepeating("updateTarget", 0f, 0.3f);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (target == null)
+            return;
+
+        Vector3 dir = target.position - transform.position;
+        Quaternion lookRotation = Quaternion.LookRotation(dir);
+
+        Vector3 rotation = Quaternion.Lerp(partToRotate.rotation, lookRotation, Time.deltaTime * turnSpeed).eulerAngles;
+        partToRotate.rotation = Quaternion.Euler(0f, rotation.y, 0f);
+	}
+
+
+    void updateTarget()
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag(enemyTag);
+
+        float shortestDistance = Mathf.Infinity;
+        GameObject nearestEnemy = null;
+
+        foreach (GameObject enemy in enemies)
+        {
+            float distanceToEnemy = Vector3.Distance(transform.position, enemy.transform.position);
+
+            if (distanceToEnemy < shortestDistance)
+            {
+                shortestDistance = distanceToEnemy;
+                nearestEnemy = enemy;
+            }
+
+        }
+
+        if (nearestEnemy != null && shortestDistance <= range)
+        {
+            target = nearestEnemy.transform;
+        }
+        else
+        {
+            target = null;
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, range);
+    }
+}
