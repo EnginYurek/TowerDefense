d77cac8542ea1f671e895536335f03285226a0e3
Engin Yurek <yurekengin@gmail.com>
2018-08-22 12:20:40 +0300

    build turret
    --Automatise building turrets
    --Add hover over animation

---
+++ Assets/Scripts/BuildManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class BuildManager : MonoBehaviour {
+
+    private GameObject turretToBuild;
+
+    public static BuildManager instance;
+
+    public GameObject standardTurretPrefab;
+
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogError("More than one build manager in the scene");
+            return;
+        }
+
+        instance = this;
+    }
+    public GameObject getTurretToBuild()
+    {
+        return turretToBuild;
+    }
+
+
+    private void Start()
+    {
+        turretToBuild = standardTurretPrefab;
+    }
+}
---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,46 @@
+﻿
+using UnityEngine;
+
+public class Node : MonoBehaviour {
+
+
+    public Color hoverColor;
+
+    private Renderer rend;
+    private Color initialColor;
+
+    private GameObject turret;
+
+    public Vector3 positionOffset;
+
+    private void Start()
+    {
+        rend = GetComponent<Renderer>();
+
+        initialColor = rend.material.color;
+    }
+    private void OnMouseEnter()
+    {
+        rend.material.color = hoverColor;
+    }
+
+
+    private void OnMouseExit()
+    {
+        rend.material.color = initialColor;
+    }
+
+    private void OnMouseDown()
+    {
+        if (turret != null)
+        {
+            Debug.Log("Hit te deck!!");
+            return;
+        }
+
+        //Build turrent
+        GameObject turretToBuild = BuildManager.instance.getTurretToBuild();
+        turret = (GameObject)Instantiate(turretToBuild, transform.position + positionOffset, transform.rotation);
+
+    }
+}
