bcbbcc55770d7a03453e3632faad79da9e7f6b13
Engin Yurek <yurekengin@gmail.com>
2018-09-05 23:59:09 +0300

    Add health bar to the enemies

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Enemy : MonoBehaviour
 {
@@ -8,19 +9,27 @@ public class Enemy : MonoBehaviour
     [HideInInspector]
     public float speed;
 
-    public float health = 100;
+    public float startHealth = 100;
+    private float health;
+
     public int worth = 50;
 
     public GameObject deathEffect;
 
+    [Header("Unity Stuff")]
+    public Image healthBar;
 
     private void Start()
     {
         speed = startSpeed;
+        health = startHealth;
     }
     public void takeDamage(float amount)
     {
         health -= amount;
+
+        healthBar.fillAmount = health / startHealth;
+
         Debug.Log("Health == " +  health);
         if (health <= 0)
         {
