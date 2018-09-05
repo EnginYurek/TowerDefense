3f450bbd63ecaa99ba81df690b157897d155847a
Engin Yurek <yurekengin@gmail.com>
2018-09-05 22:58:32 +0300

    Add MainMenu Scene, play and quit buttons with functionality

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+    
+    public string levelToLoad= "MainLevel";
+
+	public void play()
+    {
+        SceneManager.LoadScene(levelToLoad);
+        Debug.Log("Play !!");
+    }
+
+    public void quit()
+    {
+        Debug.Log("Exiting !!");
+        Application.Quit();
+    }
+}
